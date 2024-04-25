c534357c0fae03c2661ebce5d0a025473b437ca2
Jocke <joakim03moller@gmail.com>
2024-04-25 16:43:59 +0200

    Registrering och inlogging funkar. Fungerande hash också

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,6 +12,8 @@ mongoose.connect(process.env.DATABASE).then(() => {
     console.log(`Failed to connect to database: ${error}`);
 });
 
+/* User model */
+const User = require('../models/User');
 
 
 /* Lägg till användare */
@@ -25,6 +27,8 @@ router.post('/register', async (req, res) => {
         }
 
         /* Spara användare */
+        const user = new User({ username, password});
+        await user.save();
         res.status(201).json({ message: 'User created'})
 
     } catch (error) {
@@ -34,12 +38,25 @@ router.post('/register', async (req, res) => {
 
 /* Logga in användare */
 router.post('/login', async (req, res) => {
-    try{
+    try {
         const {username, password} = req.body;
 
         /* Validering */
         if (!username || !password ){ 
             return res.status(400).json({ error: 'Invalid input, send username and password'});
+        }
+
+        /* Kontrollera om användare existerar */
+        const user = await User.findOne({username});
+        if(!user) {
+            return res.status(401).json({ error: 'Fel användarnamn/lösenord'})
+        }
+
+        const isPasswordMatch = await user.comparePassword(password);
+        if(!isPasswordMatch) {
+            return res.status(401).json({ error:'Fel användarnamn/lösenord'})
+        } else {
+            res.status(200).json({ message: 'User logged in!'})
         }
 
     } catch (error) {
